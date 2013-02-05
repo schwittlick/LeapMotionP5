e573915fd372f7ccaf9b939d76b48f43dd92f8ef
Marcel Schwittlick <marzzzel@gmail.com>
2013-02-05 14:57:16 +0100

    updated leapprocessing example

--- src/example/LeapProcessing.java
+++ src/example/LeapProcessing.java
@@ -2,6 +2,7 @@ package example;
 
 import java.util.Map;
 
+import com.leapmotion.leap.Finger;
 import com.leapmotion.leap.Vector;
 import com.onformative.leap.LeapMotionP5;
 
@@ -11,52 +12,30 @@ public class LeapProcessing extends PApplet {
 
   LeapMotionP5 leap;
 
-  static float LEAP_WIDTH = 200.0f; // in mm
-  static float LEAP_HEIGHT = 700.0f; // in mm
-
   public void setup() {
     size(500, 500, P3D);
 
     leap = new LeapMotionP5(this);
-
-
   }
 
   public void draw() {
     background(0);
     lights();
     fill(255);
-    for (Map.Entry entry : leap.getFingerPositions().entrySet()) {
-      Integer fingerId = (Integer) entry.getKey();
-      Vector position = (Vector) entry.getValue();
-      noStroke();
-      ellipse(leapToScreenX(position.getX()), leapToScreenY(position.getY()), 24.0f, 24.0f);
-    }
-
-    for (Map.Entry entry : leap.getToolPositions().entrySet()) {
-      Integer toolId = (Integer) entry.getKey();
-      Vector position = (Vector) entry.getValue();
-      noStroke();
-      ellipse(leapToScreenX(position.getX()), leapToScreenY(position.getY()), 24.0f, 24.0f);
+    for (Finger finger : leap.getActiveFingers()) {
+      ellipse(leap.leapToScreenX(finger.tipPosition().getX()),
+          leap.leapToScreenY(finger.tipPosition().getY()), 20, 20);
     }
 
     translate(width / 2, height / 2);
     for (Map.Entry entry : leap.getHandPitch().entrySet()) {
       double pitch = (Double) entry.getValue();
-      System.out.println("pitch:" + pitch);
       rotateX(map((float) pitch, -1, 1, 0, TWO_PI));
     }
 
     for (Map.Entry entry : leap.getHandRoll().entrySet()) {
       double roll = (Double) entry.getValue();
-      System.out.println("roll:" + roll);
       rotateY(map((float) roll, -1, 1, 0, TWO_PI));
-    }
-
-    for (Map.Entry entry : leap.getHandYaw().entrySet()) {
-      double yaw = (Double) entry.getValue();
-      System.out.println("yaw:" + yaw);
-      // rotateY(map((float) yaw, -1, 1, 0, TWO_PI));
     }
 
 
@@ -68,22 +47,5 @@ public class LeapProcessing extends PApplet {
   public void stop() {
     leap.stop();
     super.stop();
-  }
-
-  float leapToScreenX(float x) {
-    float c = width / 2.0f;
-    if (x > 0.0) {
-      return lerp(c, width, x / LEAP_WIDTH);
-    } else {
-      return lerp(c, 0.0f, -x / LEAP_WIDTH);
-    }
-  }
-
-  float leapToScreenY(float y) {
-    return lerp(height, 0.0f, y / LEAP_HEIGHT);
-  }
-
-  public static void main(String[] args) {
-    PApplet.main(new String[] {"example.LeapProcessing"});
   }
 }
