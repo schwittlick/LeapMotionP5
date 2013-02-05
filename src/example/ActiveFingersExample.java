3ffd889e4e848d0bb9f893712142e37ea0c611c6
Marcel Schwittlick <marzzzel@gmail.com>
2013-02-05 11:56:31 +0100

    updated archive file

--- src/example/ActiveFingersExample.java
+++ src/example/ActiveFingersExample.java
@@ -8,12 +8,8 @@ import processing.core.PApplet;
 public class ActiveFingersExample extends PApplet {
   LeapMotionP5 leap;
 
-  static float LEAP_WIDTH = 200.0f; // in mm
-  static float LEAP_HEIGHT = 700.0f; // in mm
-
   public void setup() {
     size(500, 500);
-
     leap = new LeapMotionP5(this);
   }
 
@@ -21,26 +17,13 @@ public class ActiveFingersExample extends PApplet {
     background(0);
     fill(255);
     for (Finger finger : leap.getActiveFingers()) {
-      ellipse(leapToScreenX(finger.tipPosition().getX()),
-          leapToScreenY(finger.tipPosition().getY()), 10, 10);
+      ellipse(leap.leapToScreenX(finger.tipPosition().getX()),
+          leap.leapToScreenY(finger.tipPosition().getY()), 10, 10);
     }
   }
 
   public void stop() {
     leap.stop();
     super.stop();
-  }
-
-  float leapToScreenX(float x) {
-    float c = width / 2.0f;
-    if (x > 0.0) {
-      return lerp(c, width, x / LEAP_WIDTH);
-    } else {
-      return lerp(c, 0.0f, -x / LEAP_WIDTH);
-    }
-  }
-
-  float leapToScreenY(float y) {
-    return lerp(height, 0.0f, y / LEAP_HEIGHT);
   }
 }
